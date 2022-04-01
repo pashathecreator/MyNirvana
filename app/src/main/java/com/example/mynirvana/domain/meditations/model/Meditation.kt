95e18cac30f18204e58aadb87ec21dedb253fab9
skelrath <zzzkelrath@yandex.ru>
2022-04-01 17:35:17 +0700

    working with blur on bottom navigation menu

--- app/src/main/java/com/example/mynirvana/domain/meditations/model/Meditation.kt
+++ app/src/main/java/com/example/mynirvana/domain/meditations/model/Meditation.kt
@@ -2,6 +2,7 @@ package com.example.mynirvana.domain.meditations.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.mynirvana.R
 
 
 @Entity
@@ -9,6 +10,7 @@ data class Meditation(
     val header: String,
     val time: Long,
     val imageResourceId: Int,
+    val soundResourceId: Int = R.raw.rain_sound,
     @PrimaryKey(autoGenerate = true)
     val id: Int? = null
 )
--- app/src/main/java/com/example/mynirvana/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/com/example/mynirvana/presentation/mainActivity/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.example.mynirvana.presentation.mainActivity
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.fragment.NavHostFragment
 import com.example.mynirvana.R
 import com.example.mynirvana.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import eightbitlab.com.blurview.BlurView
+import eightbitlab.com.blurview.RenderScriptBlur
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +23,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val blurView = binding.blurView
+        blurBackground(blurView)
     }
+
+    private fun blurBackground(blurView: BlurView) {
+        val radius = 20f
+        val decorView = window.decorView
+        val windowBackground = decorView.background
+
+        blurView.setupWith(decorView.findViewById(android.R.id.content))
+            .setFrameClearDrawable(windowBackground)
+            .setBlurAlgorithm(RenderScriptBlur(this))
+            .setBlurRadius(radius)
+            .setBlurAutoUpdate(true)
+            .setHasFixedTransformationMatrix(true) // Or false if it's in a scrolling container or might be animated
+
+    }
+
 
 }
